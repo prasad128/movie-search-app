d43296111c72c2f0b562b4a741fee3599dcde1ef
Prasad Patil <patilprasad129@gmail.com>
2025-07-06 23:28:58 +0530

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [
-      tailwindcss(),
-    react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
-  },
-})
+    tailwindcss(),
+    react()
+  ],
+  ...(command === 'serve' && {
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['./src/test/setup.ts'],
+    },
+  }),
+}))
