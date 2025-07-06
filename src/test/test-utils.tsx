0afc8b897e5204951d545d5f87666c5fc50aa9ab
Prasad Patil <patilprasad129@gmail.com>
2025-07-06 23:12:52 +0530

    fix lint

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,43 +1,40 @@
-import { render } from '@testing-library/react'
-import type { RenderOptions } from '@testing-library/react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-import type { ReactElement } from 'react'
-import { store } from '../store'
+import { render } from "@testing-library/react";
+import type { RenderOptions } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import type { ReactElement } from "react";
+import { store } from "../store";
 
 // Create a custom render function that includes providers
-const createTestQueryClient = () => new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
+const createTestQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
     },
-  },
-})
+  });
 
-interface CustomRenderOptions extends Omit<RenderOptions, 'wrapper'> {
-  queryClient?: QueryClient
+interface CustomRenderOptions extends Omit<RenderOptions, "wrapper"> {
+  queryClient?: QueryClient;
 }
 
-const customRender = (
-  ui: ReactElement,
-  options: CustomRenderOptions = {}
-) => {
-  const { queryClient = createTestQueryClient(), ...renderOptions } = options
+const customRender = (ui: ReactElement, options: CustomRenderOptions = {}) => {
+  const { queryClient = createTestQueryClient(), ...renderOptions } = options;
 
   const Wrapper = ({ children }: { children: React.ReactNode }) => (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          {children}
-        </BrowserRouter>
+        <BrowserRouter>{children}</BrowserRouter>
       </QueryClientProvider>
     </Provider>
-  )
+  );
 
-  return render(ui, { wrapper: Wrapper, ...renderOptions })
-}
+  return render(ui, { wrapper: Wrapper, ...renderOptions });
+};
 
 // Re-export everything
-export * from '@testing-library/react'
-export { customRender as render } 
+// eslint-disable-next-line react-refresh/only-export-components
+export * from "@testing-library/react";
+export { customRender as render };
